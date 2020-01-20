273be1db0d7ce1d9dd6ffb5798838f0803dcd7c7
darius slither <arunavsingha06@gmail.com>
2020-01-20 17:15:35 +0530

    passport js

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
 const app = express();
 
 
@@ -16,20 +19,34 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());// initialise passport
+app.use(passport.session());// using passport to manage our session.
+
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
-   useUnifiedTopology: true
+  // useUnifiedTopology: true
 });
-
-const userSchema = new mongoose.Schema ({
+mongoose.set("useCreateIndex", true);
+// in order to use plugins make sure to have mongoose schema set up instead simple js object.
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-
-userSchema.plugin(encrypt, { secret:process.env.Secret, encryptedFields: ["password"] });
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy()); // must enter right below the new User model always.
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
   res.render("home");
@@ -43,35 +60,87 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+app.get("/secrets", function(req, res) {
+  if (req.isAuthenticated()){
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
 app.post("/register", function(req, res) {
-  const newUser = new User ({
-    email: req.body.username,
-    password: req.body.password
-  });
-  newUser.save(function(err) {
+
+  User.register({username: req.body.username}, req.body.password, function(err, user) {
     if (err) {
       console.log(err);
     } else {
-      res.render("secrets");
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
     }
   });
+  // bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+  //   const newUser = new User({
+  //     email: req.body.username,
+  //     password: hash
+  //   });
+  //   newUser.save(function(err) {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       res.render("secrets");
+  //     }
+  //   });
+  // });
+
 });
 
 app.post("/login", function(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
 
-  User.findOne({email: username}, function(err, foundUser) {
-    if(err) {
+  const user = new User ({
+    username: req.body.username,
+    password: req.body.password
+
+  });
+
+  req.login(user, function(err) {
+    if (err) {
       console.log(err);
     } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
-      }
-    }
-  });
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+    });
+  }
+});
+  // const username = req.body.username;
+  // const password = req.body.password;
+  //
+  // User.findOne({
+  //   email: username
+  // }, function(err, foundUser) {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     if (foundUser) {
+  //       // if (foundUser.password === password) {
+  //       // substituting to bcrypt compare
+  //       bcrypt.compare(password, foundUser.password, function(err, result) {
+  //         // res == true
+  //         if (result === true) {
+  //           res.render("secrets");
+  //         }
+  //
+  //       });
+  //
+  //       // }
+  //     }
+  //   }
+  // });
 });
 
 app.listen(3000, function() {
